20afd1fee0231701dda46d8e425b8c62030e0eb2
Kruthika K <kkruthika99@gmail.com>
2023-04-23 09:42:43 +0530

    all

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about5.png";
+import laptopImg from "../../Assets/about3.png";
 import Toolstack from "./Toolstack";
 
 function About() {
