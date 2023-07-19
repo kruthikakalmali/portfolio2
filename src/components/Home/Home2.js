35e1670878aaf7fa4b5cd6d1813f6fafbbea8cee
Kruthika K <kruthikakalmali@gmail.com>
2023-07-19 21:23:54 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -83,7 +83,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/kruuked_/"
+                  href=""
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
